17e17583927e13f173381ccbbe6dc12b57b4ff70
Ankit kumar <ak36924648@gmail.com>
2024-06-18 17:01:54 +0530

    Add files via upload

--- src/pages/PhonesAndAccessories.js
+++ src/pages/PhonesAndAccessories.js
@@ -1,11 +1,31 @@
-import React from 'react';
-import ElectronicsCategoryLayout from './ElectronicsCategoryLayout';
+// import React from 'react';
+// import ElectronicsCategoryLayout from './ElectronicsCategoryLayout';
+
+// function PhonesAndAccessories() {
+//   const category = "Phones-accessories"; // Match this with the category in your products
+//   const bannerImage = "/fashion/electronincsbanner.jpg"; // Update with correct path
+
+//   return <ElectronicsCategoryLayout category={category} bannerImage={bannerImage} />;
+// }
+
+// export default PhonesAndAccessories;
+
+
+
+
+import React, { Suspense, lazy } from 'react';
+
+const ElectronicsCategoryLayout = lazy(() => import('./ElectronicsCategoryLayout'));
 
 function PhonesAndAccessories() {
   const category = "Phones-accessories"; // Match this with the category in your products
   const bannerImage = "/fashion/electronincsbanner.jpg"; // Update with correct path
 
-  return <ElectronicsCategoryLayout category={category} bannerImage={bannerImage} />;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <ElectronicsCategoryLayout category={category} bannerImage={bannerImage} />
+    </Suspense>
+  );
 }
 
 export default PhonesAndAccessories;
