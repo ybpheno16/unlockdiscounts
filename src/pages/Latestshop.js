28dd13bd07c0a61b91c76c12943b45a9b78028ac
MR. ROGHUNATH MONDAL <141188905+6296190782@users.noreply.github.com>
2024-08-15 23:03:43 +0530

    Add files via upload

---
+++ src/pages/Latestshop.js
@@ -0,0 +1,108 @@
+import React from "react";
+import './Latestshop.css';
+
+const Latestshop = () => {
+  return (
+    <div className="banner-wrapper">
+      <h1>SHOP THE LATEST</h1>
+      <div className="wrapper">
+        <div className="images">
+          <div className="image">
+            <img src="/Latestshop/image 1.png" alt="slider" />
+            <h4>Kid's Wear</h4>
+            <p>Description of first product</p>
+          </div>
+          <div className="image">
+            <img src="/Latestshop/image 2.png" alt="slider" />
+            <h4>Women's Wear</h4>
+            <p>Description of second product</p>
+          </div>
+          <div className="image">
+            <img src="/Latestshop/image 3.png" alt="slider" />
+            <h4>Men's Wear</h4>
+            <p>Description of third product</p>
+          </div>
+          <div className="image">
+            <img src="/Latestshop/image 4.png" alt="slider" />
+            <h4>Electronic Gadgets</h4>
+            <p>Description of first product</p>
+          </div>
+          <div className="image">
+            <img src="/Latestshop/image 5.png" alt="slider" />
+            <h4>Fitness</h4>
+            <p>Description of first product</p>
+          </div>
+          <div className="image">
+            <img src="/Latestshop/image 6.png" alt="slider" />
+            <h4>Beauty Products</h4>
+            <p>Description of first product</p>
+          </div>
+          <div className="image">
+            <img src="/Latestshop/image 7.png" alt="slider" />
+            <h4>Jewellery & Accessories</h4>
+            <p>Description of first product</p>
+          </div>
+          <div className="image">
+            <img src="/Latestshop/image 8.png" alt="slider" />
+            <h4>Jewellery & Accessories</h4>
+            <p>Description of first product</p>
+          </div>
+          <div className="image">
+            <img src="/Latestshop/image 9.png" alt="slider" />
+            <h4>Jewellery & Accessories</h4>
+            <p>Description of first product</p>
+          </div>
+        </div>
+        <div className="images">
+          <div className="image">
+            <img src="/Latestshop/image 1.png" alt="slider" />
+            <h4>Kid's Wear</h4>
+            <p>Description of first product</p>
+          </div>
+          <div className="image">
+            <img src="/Latestshop/image 2.png" alt="slider" />
+            <h4>Women's Wear</h4>
+            <p>Description of second product</p>
+          </div>
+          <div className="image">
+            <img src="/Latestshop/image 3.png" alt="slider" />
+            <h4>Men's Wear</h4>
+            <p>Description of third product</p>
+          </div>
+          <div className="image">
+            <img src="/Latestshop/image 4.png" alt="slider" />
+            <h4>Electronic Gadgets</h4>
+            <p>Description of first product</p>
+          </div>
+          <div className="image">
+            <img src="/Latestshop/image 5.png" alt="slider" />
+            <h4>Fitness</h4>
+            <p>Description of first product</p>
+          </div>
+          <div className="image">
+            <img src="/Latestshop/image 6.png" alt="slider" />
+            <h4>Beauty Products</h4>
+            <p>Description of first product</p>
+          </div>
+          <div className="image">
+            <img src="/Latestshop/image 7.png" alt="slider" />
+            <h4>Jewellery & Accessories</h4>
+            <p>Description of first product</p>
+          </div>
+          <div className="image">
+            <img src="/Latestshop/image 8.png" alt="slider" />
+            <h4>Jewellery & Accessories</h4>
+            <p>Description of first product</p>
+          </div>
+          <div className="image">
+            <img src="/Latestshop/image 9.png" alt="slider" />
+            <h4>Jewellery & Accessories</h4>
+            <p>Description of first product</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Latestshop;
