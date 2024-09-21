a51c58e974e8d84d3446fda82e8e7492c317059e
Vineet Raj <vineetrajrj26@gmail.com>
2024-09-21 13:56:07 +0530

    Checking

--- src/pages/KidsWear.js
+++ src/pages/KidsWear.js
@@ -1,20 +1,19 @@
-import React, { Suspense, lazy } from 'react';
+import React, { Suspense, lazy } from "react";
 
-const lazyLoad = (Component) => (props) => (
-  <Suspense fallback={<div>Loading...</div>}>
-    <Component {...props} />
-  </Suspense>
-);
+const lazyLoad = (Component) => (props) =>
+  (
+    <Suspense fallback={<div>Loading...</div>}>
+      <Component {...props} />
+    </Suspense>
+  );
 
-const FashionLayout = lazyLoad(lazy(() => import('./LinkedPageLayout')));
+const FashionLayout = lazyLoad(lazy(() => import("./LinkedPageLayout")));
 
 function KidsWear() {
   const title = "KIDS";
   const category = "Kid's Wear";
-  return <FashionLayout category={category} title={title}  />;
+  // Checking if the category is available in the database
+  return <FashionLayout category={category} title={title} />;
 }
 
 export default KidsWear;
-
-
-
